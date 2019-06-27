d263b12c41a2fac037c286105ca2885b8afcb793
lailalelouch <lsb-95@hotmail.com>
2019-06-27 13:14:42 +0300

    solution

--- flights/views.py
+++ flights/views.py
@@ -2,7 +2,7 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdate
 from datetime import datetime
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer
+from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer
 
 
 class FlightsList(ListAPIView):
@@ -46,3 +46,7 @@ class BookFlight(CreateAPIView):
 
 	def perform_create(self, serializer):
 		serializer.save(user=self.request.user, flight_id=self.kwargs['flight_id'])
+
+
+class Register(CreateAPIView):
+	serializer_class = RegisterSerializer
